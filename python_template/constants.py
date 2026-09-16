f6833cfb852444c2a6d65384720ef46511ee8da0
androsh7@gmail.com <androsh7@gmail.com>
2026-09-15 20:19:13 -0700

    Fixed publish and tag workflow, fixed VERSION constant, and added additional linting

--- python_template/constants.py
+++ python_template/constants.py
@@ -6,3 +6,5 @@ from pathlib import Path
 
 main_file_path = getattr(sys.modules["__main__"], "__file__", None) or sys.argv[0]
 PARENT_DIRECTORY = Path(main_file_path).resolve().parents[1]
+
+VERSION = (PARENT_DIRECTORY / "VERSION.txt").read_text().strip()
